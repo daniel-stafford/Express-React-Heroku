834cf8d87b2c11f0cbc0c94e1be78cf9905bdec5
Daniel-Stafford <stafford.daniel@gmail.com>
2019-10-13 14:37:13 +0300

    initial deployment take 5

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get("/api", async (req, res) => {
 })
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.state("client/build"))
+  app.use(express.static("client/build"))
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   })
